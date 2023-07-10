13132ffaf8c62ea7424d42a8bdf3c5306e82a257
makwayne117 <mak.wayne117@gmail.com>
2023-07-09 18:55:09 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import {Container, Box} from '@chakra-ui/react'
+
+const Page = () => {
+    return (
+        <Container>
+
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I&apos;m a full stack developer in California.
+            </Box>
+
+            <div> Hello </div>
+        </Container>
+    )
+}
+export default Page
